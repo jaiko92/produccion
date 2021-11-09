6b1dd05eaf65c8c1a25c1683d7ef8e22be2ec954
LoginWeb Developer <empresa.loginweb@gmail.com>
2021-11-09 14:22:49 -0400

    update library

--- views/index.php
+++ views/index.php
@@ -10,38 +10,51 @@ function fw_index()
 
 
 ?>
-    <link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/MDB/css/mdb.min.css" rel="stylesheet">
-  
+        <meta http-equiv="pragma" content="no-cache" />
+        <script src="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/js/jquery-2.0.0.min.js" type="text/javascript"></script>
+        <script src="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+        <link href="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/fonts/fontawesome/css/fontawesome-all.min.css" type="text/css" rel="stylesheet">
+        <link href="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/css/ui.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+        <script src="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/js/script.js" type="text/javascript"></script>
+
+        <script src="<?php echo WP_PLUGIN_URL; ?>/produccion/library/notify.js" type="text/javascript"></script>
+        
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $.notify.defaults({globalPosition: 'bottom right'})
+                $('#mitabla').html("<center><img class='img-sm' src='<?php echo WP_PLUGIN_URL; ?>/produccion/resources/reload.gif'></center>");
+                $.ajax({
+                    url: "<?php echo WP_PLUGIN_URL; ?>/produccion/views/production_list.php",
+                    dataType: 'html',
+                    contentType: 'text/html',
+                    success: function (response) {
+                        $('#mitabla').html(response);	
+                    }
+                });
+            }); 
+        
+            function production_create() {
+                $('#mitabla').html("<center><img class='img-sm' src='<?php echo WP_PLUGIN_URL; ?>/produccion/resources/reload.gif'></center>");
+                $.ajax({
+                    url: "<?php echo WP_PLUGIN_URL; ?>/produccion/views/production_create.php",
+                    dataType: 'html',
+                    contentType: 'text/html',
+                    success: function (response) {
+                        $('#mitabla').html(response);	
+                    }
+                });
+            }
+        </script>
         <div class="container-fluid">
             <h1 class="h3 mb-4 text-gray-800">Producciones</h1>
 
             <div id="mitabla"></div>
         </div>
-   
-    <script src="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/MDB/js/mdb.min.js"></script>
-    <script src="<?php echo WP_PLUGIN_URL; ?>/produccion/resources/MDB/js/jquery-2.0.0.min.js"></script>
-    
+
     <script>
-        $('#mitabla').html("<center><img class='img-sm' src='<?php echo WP_PLUGIN_URL; ?>/produccion/resources/reload.gif'></center>");
-        $.ajax({
-            url: "<?php echo WP_PLUGIN_URL; ?>/produccion/views/production_list.php",
-            dataType: 'html',
-            contentType: 'text/html',
-            success: function (response) {
-                $('#mitabla').html(response);	
-            }
-        });
-        function production_create() {
-            $('#mitabla').html("<center><img class='img-sm' src='<?php echo WP_PLUGIN_URL; ?>/produccion/resources/reload.gif'></center>");
-            $.ajax({
-                url: "<?php echo WP_PLUGIN_URL; ?>/produccion/views/production_create.php",
-                dataType: 'html',
-                contentType: 'text/html',
-                success: function (response) {
-                    $('#mitabla').html(response);	
-                }
-            });
-        }
+
     </script>
 <?php
 
